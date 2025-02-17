cee124b8a71a2453bc0222a0dcd8c18b674cde2d
Aurélien VERNAY <18377871+AurelienVernay@users.noreply.github.com>
2025-02-17 15:49:35 +0100

    added Screens & router

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,40 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.ktorm.database.Database
 import org.ktorm.database.asIterable
+import routing.Router
+import routing.Routes
+import ui.HomeScreen
+import ui.LoginScreen
 
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun App(database: Database) {
+    val router = remember { Router() }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Surface {
+
+            when (router.currentRoute) {
+                Routes.LOGIN -> LoginScreen({ email, password ->
+                    println("Tentative de connexion avec $email et un mot de passe ${password}")
+//                    database.executeQuery("SELECT 1").asIterable().map {
+//                        val received = it.getInt(1);
+//                        if (received == 1) {
+//                            router.navigateTo(route = Routes.HOME)
+//                        }
+//                    }
+                })
+
+                Routes.HOME -> HomeScreen()
+            }
+
+
         }
     }
 }
@@ -30,7 +43,6 @@ fun main() = application {
 
     val database = Database.connect(
         url = "jdbc:mysql://localhost:3306/carist-si",
-        driver = "com.mysql.jdbc.Driver",
         user = "root",
         password = null
     )
@@ -38,12 +50,13 @@ fun main() = application {
     database.useConnection { connection ->
         val sql = "SELECT 1"
         connection.prepareStatement(sql).use { statement ->
-            statement.executeQuery().asIterable().map { println("it worked : " + it.getString(1))
+            statement.executeQuery().asIterable().map {
+                println("it worked : " + it.getString(1))
             }
         }
     }
 
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        App(database)
     }
 }
